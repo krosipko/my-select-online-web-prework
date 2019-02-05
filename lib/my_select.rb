23ba7754c86797024c1c54c47bef87f7e1a81057
Kyle Rosipko <krosipko@outlook.com>
2019-02-05 05:34:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,12 @@
 def my_select(collection)
-  new_collection = []
-  i = 0
-  while i < collection.length 
-    new_array
   
+  if block_given?
+    new_collection = []
+    i = 0
+    while i < collection.length 
+      yield(collection[i])
+      i += 1
+    end
+  else "No block was given"
+  end
 end
